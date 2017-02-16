189c1730b9e517b24ee90e4d6e609bad5ecfd632
eirikriv <eirik.rivedal@gmail.com>
2017-02-16 10:07:28 +0000

    Added selsnium driver and set up scraping of itslearning

---
+++ selenium/seleniumFFTest.py
@@ -0,0 +1,13 @@
+# Start up an Xvfb display
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+# Load a Firefox selenium webdriver session
+from selenium import webdriver
+browser = webdriver.Firefox()
+
+# Visit our front page
+browser.get("http://www.itslearning.no/")
+print browser.page_source
+browser.close()
