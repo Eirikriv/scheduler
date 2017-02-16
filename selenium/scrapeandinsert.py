5faefb1e59989895bfee7f16d5c374608b4d0424
eirikriv <eirik.rivedal@gmail.com>
2017-02-16 15:32:23 +0000

    Scraping itl now working, currently only updates db with first course found in courselist

---
+++ selenium/scrapeandinsert.py
@@ -0,0 +1,71 @@
+# Start up an Xvfb display that has 0 window size
+from pyvirtualdisplay import Display
+#display.sendstop() #use this to close display after running 
+# Load a Firefox selenium webdriver session
+from selenium import webdriver
+from getpass import getpass
+from database import db
+from database import Courses
+
+import time
+#import time to slow selenium down
+
+def insertIntoDatabase(studentid, courseid, coursename):
+    insertion = Courses(studentid,courseid,coursename)
+    db.session.add(insertion)
+    db.session.commit()
+
+
+
+def startdisplay():
+    display = Display(visible=0, size=(800, 600))
+    display.start()
+
+
+def scrapeItslearning():
+    driver = webdriver.Firefox()
+    u_username=raw_input("NTNU username: ")
+    u_password=getpass("NTNU password: ") 
+    driver.get('http://www.instabart.no/') 
+
+    pressend=driver.find_element_by_class_name("itslearning")
+    time.sleep(1)
+    pressend.click()
+
+    time.sleep(3)
+    its=driver.find_element_by_id("username")
+    time.sleep(1)
+    its.send_keys(u_username) 
+
+    passwd = driver.find_element_by_id("password")
+    time.sleep(1)
+    passwd.send_keys(u_password)
+
+
+    loginbutton = driver.find_element_by_class_name("submit")
+    time.sleep(1)
+    loginbutton.click()
+
+    time.sleep(4)
+
+    driver.get('https://ntnu.itslearning.com/main.aspx?TextURL=Course%2fAllCourses.aspx')
+    time.sleep(4)
+    driver.switch_to.frame(driver.find_element_by_name("mainmenu"))
+    time.sleep(4)
+    courses = driver.find_elements_by_css_selector("td > .ccl-iconlink")
+
+    singleReturnVar = ""
+
+    for course in courses:
+        singleReturnVar = course.text
+        break
+
+    driver.close()
+    return singleReturnVar
+
+def main():
+    startdisplay()
+    coursename=scrapeItslearning()
+    print(coursename)
+    insertIntoDatabase("01","01",coursename)
+main()
