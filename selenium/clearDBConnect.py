cc4283d404358db0683a5623098c262d59a5346b
Eirikriv <eirik.rivedal@gmail.com>
2017-02-28 10:58:48 +0100

    Update clearDBConnect.py

--- selenium/clearDBConnect.py
+++ selenium/clearDBConnect.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-db = create_engine('mysql://b4eb57522d3bd0:bc88385f@us-cdbr-iron-east-04.cleardb.net/heroku_f8b7f102c73b268')
+db = create_engine('mysql://"edited":"edited"@us-cdbr-iron-east-04.cleardb.net/heroku_f8b7f102c73b268')
 
 
 print(db.table_names())
