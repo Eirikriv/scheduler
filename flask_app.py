878e2573c0855f1a71c0341e358275a153496a65
eirikriv <eirik.rivedal@gmail.com>
2017-02-28 09:36:31 +0000

    added databaseconnection to heroku

--- flask_app.py
+++ flask_app.py
@@ -1,30 +1,14 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+import sys
+sys.path.insert(0, '/home/eirikriv/mysite/selenium')
+
+from readFromDatabase import createAndReturnJson
+
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="eirikriv",
-    password="hubrohubro",
-    hostname="eirikriv.mysql.pythonanywhere-services.com",
-    databasename="eirikriv$Courses",
-)
-db = SQLAlchemy(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-comments = []
+json = createAndReturnJson()
+@app.route('/')
+def hello_world():
+    return json
 
-class Comment(db.Model):
-    
-    __tablename__ = "comments"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
-
-    comments.append(request.form["contents"])
-    return redirect(url_for('index'))
---
+++ test.py
@@ -0,0 +1,7 @@
+
+import sys
+sys.path.insert(0, '/home/eirikriv/mysite/selenium')
+
+from readFromDatabase import createAndReturnJson
+
+print createAndReturnJson()
