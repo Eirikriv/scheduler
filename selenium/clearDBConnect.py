1c1f917a8d03fddeb81ac1acf82ea29f380cca8e
eirikriv <eirik.rivedal@gmail.com>
2017-02-28 10:46:03 +0000

    added to gitignore

--- selenium/clearDBConnect.py
+++ selenium/clearDBConnect.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine
+import os
+from database2 import *
 
-db = create_engine('mysql://b4eb57522d3bd0:bc88385f@us-cdbr-iron-east-04.cleardb.net/heroku_f8b7f102c73b268')
 
+username = unameHeroku
+password = passwordHeroku
+print username
+print password
 
+URI = 'mysql://'+str(username)+':'+str(password)+'@us-cdbr-iron-east-04.cleardb.net/heroku_f8b7f102c73b268'
+#db = create_engine('mysql://b4eb57522d3bd0:bc88385f@us-cdbr-iron-east-04.cleardb.net/heroku_f8b7f102c73b268')
+db = create_engine(URI)
 print(db.table_names())
 
