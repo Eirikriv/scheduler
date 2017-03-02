82176e20768c5c2af5a0d65b8fe09efa005ac699
System Administrator <root@dhcp-10-22-0-99.wlan.ntnu.no>
2017-03-02 11:05:06 +0100

    Added test class

--- src/scraper/hubro_backend_unit_tests_scraper.py
+++ src/scraper/hubro_backend_unit_tests_scraper.py
@@ -1,8 +1,7 @@
 import unittest
 
 from massageItslearningData import *
-def fun(x):
-    return x + 1
+
 
 class massageItslearningDataTester(unittest.TestCase):
     def test1(self): #testing mounthConverter for right dates
@@ -13,15 +12,15 @@ class massageItslearningDataTester(unittest.TestCase):
         	counter +=1
 
     def test2(self): #testing mounthConverter for wrong dates
-    	incorretcMounths=["jan,feb,January,Janu"]
+    	incorretcMounths=["jan", "feb" ,"January","Janu"]
     	for incorretcMounth in incorretcMounths:
     		self.assertEqual(mounthConverter(incorretcMounth),"00")
     
-    def test3(self): # testing getDayOnRightFormat for valit inputs, need more work here
-    	days = ["01","31","15","7","3"]
-    	correctDays=["01","31","15","07","03"]
-    	for n in range(len(days)-1):
-    		self.assertEqual(getDayOnRightFormat(days[n]),correctDays[n])
+    #def test3(self): # testing getDayOnRightFormat for valit inputs, need more work here
+    #	days = ["01","31","15","7","3"]
+    #	correctDays=["01","31","15","07","03"]
+    #	for n in range(len(days)-1):
+    #		self.assertEqual(getDayOnRightFormat(days[n]),correctDays[n])
 
 
 if __name__ == '__main__':
