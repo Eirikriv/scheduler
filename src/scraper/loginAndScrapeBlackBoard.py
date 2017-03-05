bf294df8577100e909b29b1e2b1e838b378b8d0f
eirikriv <eirik.rivedal@gmail.com>
2017-03-05 16:08:46 +0000

    Small changes on master

--- src/scraper/loginAndScrapeBlackBoard.py
+++ src/scraper/loginAndScrapeBlackBoard.py
@@ -4,69 +4,69 @@ from pyvirtualdisplay import Display
 import time
 
 def scrapeBlackBoard():
-	try:
-		display = Display(visible=0, size=(800, 600))
-    	display.start()
-		u_username=raw_input("NTNU BB username: ")
-    	u_password=getpass("NTNU BB password: ")
-		driver=webdriver.Chrome()
+    try:
+        display = Display(visible=0, size=(800, 600))
+        display.start()
+        u_username=raw_input("NTNU BB username: ")
+        u_password=getpass("NTNU BB password: ")
+        driver=webdriver.Chrome()
 
-		driver.get('http://www.instabart.no/') 
+        driver.get('http://www.instabart.no/') 
 
-		pressend=driver.find_element_by_id("blackboard")
-		time.sleep(1)
-		pressend.click()
+        pressend=driver.find_element_by_id("blackboard")
+        time.sleep(1)
+        pressend.click()
 
-		time.sleep(3)
-		el = driver.find_element_by_id('org')
-		for option in el.find_elements_by_tag_name('option'):
-			if(option.text == 'NTNU'):
-				option.click() 
-				break
-		pressSubmit=driver.find_element_by_id("submit")
-		pressSubmit.click()
-		time.sleep(2)
-		its=driver.find_element_by_id("username")
-		time.sleep(1)
-		its.send_keys(u_username) 
+        time.sleep(3)
+        el = driver.find_element_by_id('org')
+        for option in el.find_elements_by_tag_name('option'):
+            if(option.text == 'NTNU'):
+                option.click() 
+                break
+        pressSubmit=driver.find_element_by_id("submit")
+        pressSubmit.click()
+        time.sleep(2)
+        its=driver.find_element_by_id("username")
+        time.sleep(1)
+        its.send_keys(u_username) 
 
-		passwd = driver.find_element_by_id("password")
-		time.sleep(1)
-		passwd.send_keys(u_password)
+        passwd = driver.find_element_by_id("password")
+        time.sleep(1)
+        passwd.send_keys(u_password)
 
 
-		loginbutton = driver.find_element_by_class_name("submit")
-		loginbutton.click()
-		time.sleep(4)
-		courses = driver.find_element_by_class_name("courseListing")
-		elementList = courses.find_elements_by_tag_name("li")
-		allDeliveriesList = []
-		for n in range(0,len(elementList)):
-			
-			driver.get("https://ntnu.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_70_1")
-			time.sleep(2)
-			infoList=[]
-			courses = driver.find_element_by_class_name("courseListing")
-			elementList = courses.find_elements_by_tag_name("li")
-			courseDescription = elementList[n].text
-			elementList[n].click()
-		  	time.sleep(2)
-		 	courses = driver.find_elements_by_class_name("h-va-baseline")
-		 	driver.find_element_by_xpath('//*[@title="\xc3\x98vinger"]').click()
-		 	excersises = driver.find_element_by_id("content_listContainer")
-			singleExercises = excersises.find_elements_by_tag_name("li")
-			for n in range(len(singleExercises)):
-				assignmentsForACourse=[]
-				assignmentsForACourse.append(courseDescription +"| " + singleExercises[n].text)
-				allDeliveriesList.append(assignmentsForACourse)
-		driver.quit()
-    	display.stop()
-		return allDeliveriesList
-	except:
-		print "did not find any assignments for you on blackboard"
-		driver.quit()
-    	display.stop()
-		return None
+        loginbutton = driver.find_element_by_class_name("submit")
+        loginbutton.click()
+        time.sleep(4)
+        courses = driver.find_element_by_class_name("courseListing")
+        elementList = courses.find_elements_by_tag_name("li")
+        allDeliveriesList = []
+        for n in range(0,len(elementList)):
+            
+            driver.get("https://ntnu.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_70_1")
+            time.sleep(2)
+            infoList=[]
+            courses = driver.find_element_by_class_name("courseListing")
+            elementList = courses.find_elements_by_tag_name("li")
+            courseDescription = elementList[n].text
+            elementList[n].click()
+            time.sleep(2)
+            courses = driver.find_elements_by_class_name("h-va-baseline")
+            driver.find_element_by_xpath('//*[@title="\xc3\x98vinger"]').click()
+            excersises = driver.find_element_by_id("content_listContainer")
+            singleExercises = excersises.find_elements_by_tag_name("li")
+            for n in range(len(singleExercises)):
+                assignmentsForACourse=[]
+                assignmentsForACourse.append(courseDescription +"| " + singleExercises[n].text)
+                allDeliveriesList.append(assignmentsForACourse)
+        driver.quit()
+        display.stop()
+        return allDeliveriesList
+    except:
+        print "did not find any assignments for you on blackboard"
+        driver.quit()
+        display.stop()
+        return None
 def isNumber(s):
     try:
         float(s)
@@ -96,16 +96,16 @@ def getDateOnRightFormat(day):
     return returnVar
 
 def parceFromBlackBoardToDatabase(doubleScrapeListFromBB):
-	returnList = []
-	for deliveries in doubleScrapeListFromBB:
-		deliveries = deliveries[0]
-		course = deliveries.split("|")[0]
-		try:
-			deadLineDate = deliveries.split("Deadline")[1]
-			returnList.append(course+"|"+deadLineDate)
-		except:
-			continue
-	return returnList
+    returnList = []
+    for deliveries in doubleScrapeListFromBB:
+        deliveries = deliveries[0]
+        course = deliveries.split("|")[0]
+        try:
+            deadLineDate = deliveries.split("Deadline")[1]
+            returnList.append(course+"|"+deadLineDate)
+        except:
+            continue
+    return returnList
 
 #in ['TDT4145 Datamodellering og databasesystemer (2017 V\xc5R)| 26. Jan 23.59.',
 def monthConverter(monthAsString):
@@ -120,37 +120,36 @@ def monthConverter(monthAsString):
     return str(returnVar)
 
 def convertTimeToRightFormate(timeString):
-	temp=""
-	for n in timeString:
-		if(isNumber(n)):
-			temp=temp+n
-	return temp[0:2]+":"+temp[2:4]+":"+"00"
+    temp=""
+    for n in timeString:
+        if(isNumber(n)):
+            temp=temp+n
+    return temp[0:2]+":"+temp[2:4]+":"+"00"
 
 def inScrapeOutListWithCourseAndDeliveryDate(scrape):
-	trimmedScrape = parceFromBlackBoardToDatabase(scrape)
-	returnList=[]
-	counter = 1
-	for entries in trimmedScrape:
-		tempList = []
-		courseName = entries.split("(")[0]
-		dateString = entries.split("|")[1]
-		day = dateString.split()[0]
-		daySplit=day.split(".")[0]
-		tempList.append("Assignment " + str(counter))
-		tempList.append(courseName)
-		day = getDateOnRightFormat(daySplit)
-		mounth = getDateOnRightFormat(monthConverter(dateString.split()[1]))
-		year = entries.split("(")[1][0:4]
-		tempList.append(year+"-"+mounth+"-"+day)
-		counter +=1
-		time = dateString.split(" ")[3]
-		time = convertTimeToRightFormate(time)
-		tempList.append(time)
-		returnList.append(tempList)
-	return returnList
+    trimmedScrape = parceFromBlackBoardToDatabase(scrape)
+    returnList=[]
+    counter = 1
+    for entries in trimmedScrape:
+        tempList = []
+        courseName = entries.split("(")[0]
+        dateString = entries.split("|")[1]
+        day = dateString.split()[0]
+        daySplit=day.split(".")[0]
+        tempList.append("Assignment " + str(counter))
+        tempList.append(courseName)
+        day = getDateOnRightFormat(daySplit)
+        mounth = getDateOnRightFormat(monthConverter(dateString.split()[1]))
+        year = entries.split("(")[1][0:4]
+        tempList.append(year+"-"+mounth+"-"+day)
+        counter +=1
+        time = dateString.split(" ")[3]
+        time = convertTimeToRightFormate(time)
+        tempList.append(time)
+        returnList.append(tempList)
+    return returnList
 
 def scrapeAndGetRawReadyForDatabase():
-	scrape=scrapeBlackBoard()
-	return inScrapeOutListWithCourseAndDeliveryDate(scrape)
-
-
+    scrape=scrapeBlackBoard()
+    return inScrapeOutListWithCourseAndDeliveryDate(scrape)
+scrapeAndGetRawReadyForDatabase()
